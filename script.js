b7d520f6c1b9a3c0b3cafa0d80cc415fcf5dd21e
leo3tian <87582530+leo3tian@users.noreply.github.com>
2023-10-30 15:14:45 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction(), scrollFunction2()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.height = "30px";
+    document.getElementById("header").style.backgroundColor = "rgb(50, 150, 50)";
   } else {
     document.getElementById("header").style.height = "75px";
+    document.getElementById("header").style.backgroundColor = "rgb(80, 180, 80)";
   }
 }
